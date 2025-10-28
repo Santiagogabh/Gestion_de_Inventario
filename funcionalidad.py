f19e6dfac1b330db969f6ff8a06235c601cdace1
Santiagogabh <santiagogabhernandez@gmail.com>
2025-10-28 11:39:27 -0500

    primer programa python

---
+++ funcionalidad.py
@@ -0,0 +1,5 @@
+def ganancia (costo_producto,precio_de_venta):
+    costo_producto = int(input("Ingrese el costo del producto: "))
+    precio de venta = int (input("Ingrese el precio de venta: "))
+
+    return costo_producto - precio_de_venta
